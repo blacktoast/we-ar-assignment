47850babbaf9aef9d0ab2c2da2b53cb690b73e21
blacktoast <dbstjd100@naver.com>
2022-08-05 21:49:15 +0900

    ✨ Feat/ 임의 각도로 공 움직임 구현

--- src/ball.ts
+++ src/ball.ts
@@ -3,20 +3,39 @@
 
 export class Ball {
   ctx: CanvasRenderingContext2D;
-
+  radius: number;
+  x: number;
+  y: number;
+  speed: number;
+  an: number;
   constructor(
     ctx: CanvasRenderingContext2D,
     radius: number,
     x: number,
-    y: number
+    y: number,
+    speed: number
   ) {
     this.ctx = ctx;
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speed = speed;
+    this.an = Math.random() * (Math.PI * 2);
+  }
+
+  draw() {
     this.ctx.beginPath();
-    this.ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
     this.ctx.fillStyle = "black";
     this.ctx.fill();
     this.ctx.closePath();
   }
 
-  move() {}
+  //초당 움직이는 픽셀은 frame * 프레임당 움직일 픽셀= 초당 픽셀
+  move(dx: number, dy: number, delta: number) {
+    this.x += 200 * (delta / 1000) * Math.cos(this.an);
+    this.y += 200 * (delta / 1000) * Math.sin(this.an);
+
+    this.draw();
+  }
 }
