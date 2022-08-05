91df3131f720d9d0adb5400bda073a9f4c323df5
blacktoast <dbstjd100@naver.com>
2022-08-05 21:48:46 +0900

    ✨ Feat/ 델타타임 적용

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,33 @@ const ctx = canvas?.getContext("2d");
 
 const run = () => {
   const ballNumber = getRandom10to20();
+  const balls: Ball[] = [];
   if (ctx) {
     Array(ballNumber)
       .fill(0)
       .forEach(() => {
         const radaius = getRandom10to20();
         const [x, y] = getRandomXY();
-        new Ball(ctx, radaius, x, y);
+        const a = new Ball(ctx, radaius, x, y, 100);
+        balls.push(a);
+        // a.draw();
       });
+    const a = (delta: number) => {
+      balls.forEach((ball) => {
+        ball.move(200, 200, delta);
+      });
+    };
+
+    let lastTick = performance.now();
+    const play = (tick: number) => {
+      const delta = tick - lastTick;
+      lastTick = tick;
+      ctx.clearRect(0, 0, canvas?.width, canvas?.height);
+      a(delta);
+      requestAnimationFrame(play);
+    };
+
+    requestAnimationFrame(play);
   }
 };
 
