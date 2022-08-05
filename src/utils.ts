dc63011a7e3cfe0abe6b1ba9d429b61dd579b89e
blacktoast <dbstjd100@naver.com>
2022-08-05 13:23:05 +0900

    ✨ Feat/ random 함수 구현

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+const random = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+export const getRandom10to20 = () => {
+  return random(10, 20);
+};
+
+export const getRandomXY = () => {
+  return [random(20, 980), random(20, 480)];
+};
